9f7d1cb989b2ae7e704f194f311d71162ba7eb80
PYramid8955 <cs.is.coding@gmail.com>
2023-10-05 18:53:03 +0300

    Add the code I made so far. - It scrapes a random link from Unsplash on the specified query.

---
+++ Wpch_App_Linux/Packages/getUnsplImg.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+from time import sleep
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:84.0) Gecko/20100101 Firefox/84.0",
+    "Accept": "*/*"
+}
+
+def get_nofollow_links(url):
+    try:
+        response = requests.get(url, headers=HEADERS)
+        response.raise_for_status()  # Raise an HTTPError for bad requests
+        soup = BeautifulSoup(response.text, 'html.parser')
+        nofollow_links = soup.find_all('a', rel='nofollow')
+        return [link.get('href') for link in nofollow_links]
+    except requests.exceptions.HTTPError as errh:
+        print(f"HTTP Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+    except requests.exceptions.RequestException as err:
+        print(f"Request Exception: {err}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    return []
+
+def main(search_query):
+    try:
+        url = f'https://unsplash.com/s/photos/{search_query}?orientation=landscape'
+        nofollow_links = get_nofollow_links(url)
+        if nofollow_links:
+            chosen_link = choice(nofollow_links)
+            print(chosen_link)
+        else:
+            print("No 'nofollow' links found on the page.")
+    except KeyboardInterrupt:
+        print("\nOperation interrupted by the user.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
