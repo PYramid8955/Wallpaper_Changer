15ad9a33dd1ef68cde0cda8f620a80b0b79816ea
PYramid8955 <cs.is.coding@gmail.com>
2023-10-05 21:06:50 +0300

    Add a way to easily and efficiently remove the app if users wishes.

---
+++ Wpch_App_Linux/remove.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+if input("\u001b[31mYou are sure that you want to delete Wallpaper_Changer?\u001b[0m \u001b[33m(Y/N)\u001b[0m: ").strip().lower() == "y":
+	home = os.path.expanduser('~')
+	os.remove(f"{home}/.config/autostart/wpch.desktop")
+	shutil.rmtree(f"{home}/.WallpaperCh")
+	print("\nDone! Wallpaper_Changer was successfully deleted from your system. If you change your mind you can download it at any time from github/PYramid8955.")
+	quit()
